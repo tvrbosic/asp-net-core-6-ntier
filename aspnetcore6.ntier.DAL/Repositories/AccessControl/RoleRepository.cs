9df8e79abf0689bc80e5693e35573d26cdec9062
tvrbosic <tvrbosic@koncar-institut.hr>
2024-03-26 14:47:51 +0100

    WIP

--- aspnetcore6.ntier.DAL/Repositories/AccessControl/RoleRepository.cs
+++ aspnetcore6.ntier.DAL/Repositories/AccessControl/RoleRepository.cs
@@ -27,30 +27,9 @@ namespace aspnetcore6.ntier.DAL.Repositories.AccessControl
 
         public async  Task Update(Role role, List<int> permissionIds)
         {
-            // Remove permissions that are not in the provided list
-            //List<Permission> permissionsToRemove = role.Permissions.Where(rp => !permissionIds.Contains(rp.Id)).ToList();
-            //foreach (var permission in role.Permissions)
-            //{
-            //    role.Permissions.Remove(permission);
-            //}
-            
-            
+            // Clear             
             role.Permissions.Clear();
-            //foreach (Permission permission in role.Permissions.ToList())
-            //{
-            //    role.Permissions.Remove(permission);
-            //}
-            //await _context.SaveChangesAsync();
 
-            // Add missing permissions
-            //List<Permission> permissionsToAdd = _context.Permissions.Where(p => permissionIds.Contains(p.Id)).ToList();
-            //foreach (var permission in permissionsToAdd)
-            //{
-            //    if (!role.Permissions.Any(rp => rp.Id == permission.Id))
-            //    {
-            //        role.Permissions.Add(permission);
-            //    }
-            //}
 
             foreach (int permissionId in permissionIds)
             {
