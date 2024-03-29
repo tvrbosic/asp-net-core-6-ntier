903acf7388fdf316bff306c70ce61405eb9d1ce6
tvrbosic <tomislav.vrbosic@gmail.com>
2024-03-29 15:52:47 +0100

    WIP

--- aspnetcore6.ntier.API/Controllers/AccessControl/PermissionController.cs
+++ aspnetcore6.ntier.API/Controllers/AccessControl/PermissionController.cs
@@ -1,5 +1,8 @@
-﻿using aspnetcore6.ntier.BLL.Services.AccessControl.DTOs;
+﻿using aspnetcore6.ntier.API.Responses;
+using aspnetcore6.ntier.BLL.Services.AccessControl;
+using aspnetcore6.ntier.BLL.Services.AccessControl.DTOs;
 using aspnetcore6.ntier.BLL.Services.AccessControl.Interfaces;
+using aspnetcore6.ntier.DAL.Models.AccessControl;
 using Microsoft.AspNetCore.Mvc;
 
 namespace aspnetcore6.ntier.API.Controllers.AccessControl
@@ -19,35 +22,39 @@ namespace aspnetcore6.ntier.API.Controllers.AccessControl
         public async Task<ActionResult<IEnumerable<PermissionDTO>>> GetPermissions()
         {
             IEnumerable<PermissionDTO> permissions = await _permissionService.GetPermissions();
-            return Ok(permissions);
+            var response = new ApiDataResponse<IEnumerable<PermissionDTO>>(permissions, "Permissions retrieved succcessfully.");
+            return Ok(response);
         }
 
         [HttpGet("{id}")]
         public async Task<ActionResult<PermissionDTO>> GetPermission(int id)
         {
             PermissionDTO permission = await _permissionService.GetPermission(id);
-            return Ok(permission);
+            var response = new ApiDataResponse<PermissionDTO>(permission, "Permission retrieved succcessfully.");
+            return Ok(response);
         }
-
         [HttpPost]
         public async Task<IActionResult> PostPermission(AddPermissionDTO permissionDTO)
         {
             await _permissionService.AddPermission(permissionDTO);
-            return Ok();
+            var response = new ApiBaseResponse("Permission created succcessfully.");
+            return Ok(response);
         }
 
         [HttpPut]
         public async Task<IActionResult> PutPermission(UpdatePermissionDTO permissionDTO)
         {
             await _permissionService.UpdatePermission(permissionDTO);
-            return Ok();
+            var response = new ApiBaseResponse("Permission updated succcessfully.");
+            return Ok(response);
         }
 
         [HttpDelete]
         public async Task<IActionResult> DeletePermission(int id)
         {
             await _permissionService.DeletePermission(id);
-            return Ok();
+            var response = new ApiBaseResponse("Permission deleted succcessfully.");
+            return Ok(response);
         }
     }
 }
--- aspnetcore6.ntier.API/Controllers/AccessControl/RoleController.cs
+++ aspnetcore6.ntier.API/Controllers/AccessControl/RoleController.cs
@@ -1,5 +1,9 @@
-﻿using aspnetcore6.ntier.BLL.Services.AccessControl.DTOs;
+﻿using aspnetcore6.ntier.API.Responses;
+using aspnetcore6.ntier.BLL.Services.AccessControl.DTOs;
 using aspnetcore6.ntier.BLL.Services.AccessControl.Interfaces;
+using aspnetcore6.ntier.BLL.Services.General.DTOs;
+using aspnetcore6.ntier.DAL.Models.AccessControl;
+using aspnetcore6.ntier.DAL.Models.General;
 using Microsoft.AspNetCore.Mvc;
 
 namespace aspnetcore6.ntier.API.Controllers.AccessControl
@@ -19,35 +23,40 @@ namespace aspnetcore6.ntier.API.Controllers.AccessControl
         public async Task<ActionResult<IEnumerable<RoleDTO>>> GetRoles()
         {
             IEnumerable<RoleDTO> roles = await _roleService.GetRoles();
-            return Ok(roles);
+            var response = new ApiDataResponse<IEnumerable<RoleDTO>>(roles, "Roles retrieved succcessfully.");
+            return Ok(response);
         }
 
         [HttpGet("{id}")]
         public ActionResult<RoleDTO> GetRole(int id)
         {
             RoleDTO role = _roleService.GetRole(id);
-            return Ok(role);
+            var response = new ApiDataResponse<RoleDTO>(role, "Role retrieved succcessfully.");
+            return Ok(response);
         }
 
         [HttpPost]
         public async Task<IActionResult> PostRole(AddRoleDTO roleDTO)
         {
             await _roleService.AddRole(roleDTO);
-            return  Ok();
+            var response = new ApiBaseResponse("Role created succcessfully.");
+            return Ok(response);
         }
 
         [HttpPut]
         public async Task<IActionResult> PutRole(UpdateRoleDTO roleDTO)
         {
             await _roleService.UpdateRole(roleDTO);
-            return  Ok();
+            var response = new ApiBaseResponse("Role updated succcessfully.");
+            return Ok(response);
         }
 
         [HttpDelete]
         public async Task<IActionResult> DeleteRole(int id)
         {
             await _roleService.DeleteRole(id);
-            return  Ok();
+            var response = new ApiBaseResponse("Role deleted succcessfully.");
+            return Ok(response);
         }
     }
 }
